67856a3cdab868172b6b70e63e08316a427cdd81
Mathieu AGOSTINI <94069017+Kantus8@users.noreply.github.com>
2024-05-24 12:34:02 +0200

    IA debut

---
+++ Assets/Scripts/IAController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum IAState
+{
+    None,
+    Idle,
+    Patrol,
+    PlayerSeen,
+    PlayerNear,
+}
+public class IAController : MonoBehaviour
+{
+    public IAState _state = IAState.None;
+    [SerializeField] private Animator _animator;
+    public bool PlayerNear = false;
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private GameObject _waypoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTransition();
+        Behaviour();
+    }
+
+    private void Behaviour()
+    {
+        switch(_state)
+        {
+            case IAState.None:
+                //
+                //
+                break;
+            case IAState.Idle:
+                //
+                //
+                break;
+            case IAState.Patrol:
+                //
+                //
+                break;
+            case IAState.PlayerSeen:
+                //
+                _agent.SetDestination(_waypoint.transform.position);
+                if (_agent.remainingDistance == 50)
+                {
+
+                }
+                //
+                break;
+            case IAState.PlayerNear:
+                //
+                //
+                break;
+        }
+    }
+
+    private void CheckTransition()
+    {
+        switch (_state)
+        {
+            case IAState.None:
+                break;
+            case IAState.Idle:
+                if (PlayerNear)
+                {
+                    _state = IAState.PlayerNear;
+                    _animator.SetBool("isPlayerClose", true);
+                }
+                break;
+            case IAState.Patrol:
+                if (PlayerNear)
+                {
+                    _state = IAState.PlayerNear;
+                    _animator.SetBool("isPlayerClose", true);
+                }
+                break;
+            case IAState.PlayerSeen:
+                if (PlayerNear)
+                {
+                    _state = IAState.PlayerNear;
+                    _animator.SetBool("isPlayerClose", true);
+                }
+                break;
+            case IAState.PlayerNear:
+                if (!PlayerNear)
+                {
+                    _state = IAState.PlayerSeen;
+                    _animator.SetBool("isPlayerClose", false);
+
+                }
+                break;
+        }
+    }
+}
